276ff917bdc8343a44a9339f81831459f57fcffa
“Evgenii <eabaulin@edu.hse.ru>
2023-10-22 18:17:00 +0300

    Lesson 3

---
+++ Python/Lesson 3/main.py
@@ -0,0 +1,28 @@
+# b = ["Кот", "Пёс", "УткоНос"]
+#
+# a = map(str.lower, b)
+#
+# for i in range(len(b)):
+# 	b[i] = b[i].lower
+#
+# n = n + 1
+# n += 1
+# n += 100
+#
+# print(n + 100, )
+#
+
+# students = [['Masha', 5], ['Petya', 4]]
+#
+# for st in students:
+# 	print(st)
+# 	for item in st:
+# 		print(item)
+#
+# 	print('-' * 9)
+
+# names = 'Olya, Anya, Petya'.split(', ')
+# print(names)
+# print(' lol '.join(names))
+
+
